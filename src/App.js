3015859ff8fffa923181da2c295781ffb60524fe
vogtn <nicolasjvogt@gmail.com>
2017-02-02 09:42:12 -0800

    placing input field

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ class App extends Component {
           entered={scaleDown.entered}
           leaved={scaleDown.leaved}
           columnWidth={150}>
+        <div className="searchField">
+          <input placeholder="Enter Movie"></input>
+          <button>Search</button>
+        </div>
         <div key="key1">Item 1</div>
         <div key="key2">Item 2</div>
         <div key="key3">Item 3</div>
